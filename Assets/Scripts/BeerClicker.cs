e54f0894b524848642e5dd710a7c5508cb1e0220
Alex <alexis.amand@gmail.com>
2024-09-30 20:10:33 +0200

    first commit

---
+++ Assets/Scripts/BeerClicker.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using Unity.VisualScripting;
+
+public class BeerClicker : MonoBehaviour
+{
+    public int beersCollected = 0; // Compteur de bières collectées
+    public Text beersText; // Référence au texte UI pour le compteur
+    public Text specialBeerText; // Référence au texte UI pour la bière spéciale
+    public List<string> specialBeers = new List<string> { "Beer-illiant", "Cheers to Beer", "Beer-lievable", "Beer-rito", "Beer-thday", "Beer-ocity" }; // Liste des bières spéciales
+
+    public GameObject plusOnePrefab; // Référence au prefab pour "+1"
+
+    private void Start()
+    {
+        specialBeerText.gameObject.SetActive(false); // Masque le texte de la bière spéciale au démarrage
+    }
+
+    private void OnMouseDown()
+    {
+        beersCollected++; // Augmente le compteur de bières
+        beersText.text = "Drunk beers : " + beersCollected; // Met à jour le texte affiché
+
+        // Vérifie si une bière spéciale est trouvée
+        if (UnityEngine.Random.value < 0.1f) // 10% de chance d'obtenir une bière spéciale
+        {
+            int randomIndex = UnityEngine.Random.Range(0, specialBeers.Count);
+            string specialBeer = specialBeers[randomIndex];
+            Debug.Log("You drink a " + specialBeer + " !");
+            StartCoroutine(ShowSpecialBeerMessage(specialBeer)); // Lance la coroutine pour afficher le message
+        }
+
+        // Affiche "+1" à l'endroit du curseur
+        ShowPlusOneEffect(Input.mousePosition);
+    }
+
+    private void ShowPlusOneEffect(Vector3 position)
+    {
+        // Instancie le prefab "+1"
+        GameObject plusOne = Instantiate(plusOnePrefab);
+        RectTransform rectTransform = plusOne.GetComponent<RectTransform>();
+        rectTransform.SetParent(GameObject.Find("Canvas").transform, false); // Assure-toi que le "+1" est sous le Canvas
+
+        // Convertit la position de la souris de l'espace écran à l'espace local du Canvas
+        Vector2 screenPoint = position;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform.parent as RectTransform, screenPoint, null, out Vector2 localPoint);
+        rectTransform.anchoredPosition = localPoint; // Positionne le "+1" à l'endroit du curseur
+
+        // Lance la coroutine pour faire disparaître le "+1"
+        StartCoroutine(MovePlusOne(plusOne));
+    }
+
+    private IEnumerator MovePlusOne(GameObject plusOne)
+    {
+        float duration = 4f; // Durée du mouvement
+        float elapsedTime = 0f;
+
+        Vector3 startPos = plusOne.transform.position;
+        Vector3 targetPos = startPos + new Vector3(0, 50, 0); // Déplace vers le haut
+
+        // Boucle d'animation
+        while (elapsedTime < duration)
+        {
+            plusOne.transform.position = Vector3.Lerp(startPos, targetPos, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        Destroy(plusOne); // Détruit l'objet après la fin de l'animation
+    }
+
+    // Coroutine pour afficher le message de bière spéciale
+    private IEnumerator ShowSpecialBeerMessage(string specialBeer)
+    {
+        specialBeerText.text = "You've drunk a " + specialBeer; // Met à jour le texte de la bière spéciale
+        specialBeerText.gameObject.SetActive(true); // Assure-toi que le texte est visible
+
+        yield return new WaitForSeconds(1f); // Attend 3 secondes
+
+        specialBeerText.gameObject.SetActive(false); // Masque le texte après 3 secondes
+    }
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+# Beer Clicker
+
+I've finally decided to learn Unity !
+
+For fun, I started developing a “Banana” clone, but with beers.
+
+For now, it's just a cup you click on.
+
+# Screenshots
+
+![ScreenShot](https://github.com/AlexisAmand/Banana-Clicker/blob/main/screenshots/beer-01.jpg)
+![ScreenShot](https://github.com/AlexisAmand/Banana-Clicker/blob/main/screenshots/beer-02.jpg)
+
+##  My config
+
+* Windows 11
+* Visual Studio 17.8.6
+* Visual Studio Code 1.93.1
+* Unity 2021.3.31f1
+
+## Did you find this page useful ?
+Why not thank me with a cup of coffee?
+
+<a href="https://www.buymeacoffee.com/alexisamand" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-blue.png" alt="Buy Me A Coffee" width="210" ></a>
+
+
+
+
+
